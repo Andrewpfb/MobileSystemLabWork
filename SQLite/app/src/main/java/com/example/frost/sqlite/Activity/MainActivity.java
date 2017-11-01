214aa32cf0712fed5eadaa263d2648948f834921
andrew.golik <andrewgolik29@gmail.com>
2017-11-02 00:07:22 +0300

    finish SQLite

--- SQLite/app/src/main/java/com/example/frost/sqlite/Activity/MainActivity.java
+++ SQLite/app/src/main/java/com/example/frost/sqlite/Activity/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.frost.sqlite.Activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.example.frost.sqlite.HelperPackage.DatabaseHelper;
 import com.example.frost.sqlite.HelperPackage.ProductAdapter;
@@ -13,23 +15,40 @@ import com.example.frost.sqlite.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<Product> products;
+    private ProgressBar progressBar;
+    private ListView listView;
     DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = new DatabaseHelper(this);
-        products = db.getAllProducts();
-        if(products==null) {
+        listView = (ListView) findViewById(R.id.MA_ProductsList);
+        progressBar = (ProgressBar) findViewById(R.id.MA_progressBar);
+        /*try {
+            ShowFunction();
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        ListTask listTask = new ListTask();
+        listTask.execute();
+    }
+
+    private void ShowFunction() throws InterruptedException {
+        products=db.getAllProducts();
+        if(products==null){
             products = new ArrayList<>();
         }
-        ListView listView = (ListView) findViewById(R.id.MA_ProductsList);
-        ProductAdapter productAdapter = new ProductAdapter(this,R.layout.item,products, db);
+        ProductAdapter productAdapter = new ProductAdapter(getApplicationContext(),R.layout.item,products,db);
         listView.setAdapter(productAdapter);
+
     }
 
     public void CreateClick(View view){
@@ -45,5 +64,36 @@ public class MainActivity extends AppCompatActivity {
     public void FavoriteClick(View view){
         Intent intent = new Intent(this,FavoriteAct.class);
         startActivity(intent);
+    }
+
+    class ListTask extends AsyncTask<Void,Integer,List<Product>>{
+
+        @Override
+        protected void onPostExecute(List<Product> result){
+            ProductAdapter productAdapter = new ProductAdapter(getApplicationContext(),R.layout.item,products,db);
+            listView.setAdapter(productAdapter);
+        }
+        @Override
+        protected List<Product> doInBackground(Void... params) {
+            try {
+                int counter = 0;
+                for(int i=0;i<5;i++) {
+                    TimeUnit.SECONDS.sleep(1);
+                    counter+=20;
+                    publishProgress(counter);
+                }
+                products = db.getAllProducts();
+                if (products == null) {
+                    products = new ArrayList<>();
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            return products;
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values){
+            progressBar.setProgress(values[0]);
+        }
     }
 }
